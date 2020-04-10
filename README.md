7498480591d9aca1e942e633b455d5aa10f88aaa
Peter Martin <github@pe7er.com>
2020-04-10 17:35:40 +0200

    version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# hcaptcha
+# hCaptcha
 A simple CAPTCHA Plugin using the hCaptcha service to protect your Joomla site against SPAM
+
+Uses the https://www.hcaptcha.com/ CAPTCHA service
---
+++ hcaptcha.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Captcha
+ *
+ * @author      Peter Martin
+ * @copyright   Copyright 2016-2017 Peter Martin. All rights reserved.
+ * @license     GNU General Public License version 2 or later
+ * @link        https://db8.eu
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\Utilities\IpHelper;
+
+/**
+ * hCaptcha Plugin
+ * Using the https://www.hcaptcha.com/ CAPTCHA service
+ *
+ * @since  1.0.0
+ */
+class PlgCaptchaHcaptcha extends CMSPlugin
+{
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var    boolean
+	 * @since  1.0.0
+	 */
+	protected $autoloadLanguage = true;
+
+	/**
+	 * Initialise the captcha
+	 *
+	 * @return  boolean    True on success, false otherwise
+	 *
+	 * @since   1.0.0
+	 * @throws  Exception
+	 */
+	public function onInit()
+	{
+		// If there is no Public Key set, then this plugin is no use, so exit
+		if ($this->params->get('publicKey', '') === '')
+		{
+			throw new \RuntimeException(Text::_('PLG_HCAPTCHA_ERROR_NO_PUBLIC_KEY'));
+		}
+
+		// Load the JavaScript from hCaptcha
+		HTMLHelper::_('script', 'https://hcaptcha.com/1/api.js', ['version' => 'auto', 'relative' => true], ['defer' => 'defer']);
+
+		return true;
+	}
+
+	/**
+	 * Gets the challenge HTML
+	 *
+	 * @param   string  $name   The name of the field. Not Used.
+	 * @param   string  $id     The id of the field.
+	 * @param   string  $class  The class of the field.
+	 *
+	 * @return  string  The HTML to be embedded in the form.
+	 *
+	 * @since  1.0.0
+	 */
+	public function onDisplay($name = null, $id = 'hcaptcha', $class = '')
+	{
+		$dom = new \DOMDocument('1.0', 'UTF-8');
+		$ele = $dom->createElement('div');
+		$ele->setAttribute('id', $id);
+		$ele->setAttribute('class', 'h-captcha');
+		$ele->setAttribute('data-sitekey', $this->params->get('publicKey', ''));
+
+		$dom->appendChild($ele);
+
+		return $dom->saveHTML($ele);
+	}
+
+	/**
+	 * Calls an HTTP POST function to verify if the user's guess was correct
+	 *
+	 * @param   string  $code  Answer provided by user. Not needed for the Hcaptcha implementation
+	 *
+	 * @return  boolean
+	 * @since   1.0.0
+	 * @throws  Exception
+	 */
+	public function onCheckAnswer($code = null)
+	{
+		$input            = Factory::getApplication()->input;
+		$privateKey       = $this->params->get('privateKey');
+		$remoteIp         = IpHelper::getIp();
+		$hCaptchaResponse = $input->get('h-captcha-response', '', 'cmd');
+
+		if (empty($privateKey))
+		{
+			throw new \RuntimeException(Text::_('PLG_HCAPTCHA_ERROR_NO_PRIVATE_KEY'));
+		}
+
+		if (empty($remoteIp))
+		{
+			throw new \RuntimeException(Text::_('PLG_HCAPTCHA_ERROR_NO_IP'));
+		}
+
+		if (empty($hCaptchaResponse))
+		{
+			throw new \RuntimeException(Text::_('PLG_HCAPTCHA_ERROR_EMPTY_SOLUTION'));
+		}
+
+		$verifyResponse = file_get_contents(
+			'https://hcaptcha.com/siteverify?secret=' . $privateKey .
+			'&response=' . $hCaptchaResponse .
+			'&remoteip=' . $remoteIp
+		);
+		$responseData   = json_decode($verifyResponse);
+
+		if ($responseData->success)
+		{
+			return true;
+		}
+
+		throw new \RuntimeException(Text::_('PLG_HCAPTCHA_ERROR_INCORRECT_CAPTCHA'));
+	}
+}
