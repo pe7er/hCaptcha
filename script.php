5d11c45adfb06d57a106b1b115bf6f79868d0f3d
Tobias Zulauf <zero-24@users.noreply.github.com>
2020-04-22 16:45:19 +0200

    Create script.php

---
+++ script.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * HttpHeader Plugin
+ *
+ * @copyright  Copyright (C) 2017 - 2019 Tobias Zulauf All rights reserved.
+ * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Installer\InstallerScript;
+
+/**
+ * Installation class to perform additional changes during install/uninstall/update
+ *
+ * @since  1.0
+ */
+class PlgSystemHttpHeaderInstallerScript extends InstallerScript
+{
+	/**
+	 * Extension script constructor.
+	 *
+	 * @since   1.0.1
+	 */
+	public function __construct()
+	{
+		// Define the minumum versions to be supported.
+		$this->minimumJoomla = '3.8';
+		$this->minimumPhp    = '7.0';
+	}
+
+	/**
+	 * Function to perform changes during postflight
+	 *
+	 * @param   string            $type    The action being performed
+	 * @param   ComponentAdapter  $parent  The class calling this method
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0.1
+	 */
+	public function postflight($type, $parent)
+	{
+		$this->removeFiles();
+	}
+}
